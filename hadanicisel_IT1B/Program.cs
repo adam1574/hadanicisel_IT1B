521b03e417b8d9be115244248c8528bbfa58c7fb
Sivera.Adam <sivera.adam@seznam.cz>
2021-11-16 11:05:55 +0100

    SKORO DONE

--- hadanicisel_IT1B/Program.cs
+++ hadanicisel_IT1B/Program.cs
@@ -6,7 +6,40 @@ namespace hadanicisel_IT1B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Random random = new Random();
+            int Nahcislo = random.Next(1, 100);
+            int tipcislo;
+            bool neuhodl = true;
+            Console.WriteLine("hádání čísla");
+            Console.WriteLine("..............");
+            Console.WriteLine($"myslim si cislo od 1 do 100");
+            while (true)
+
+            {
+                
+                Console.WriteLine("tipni hodnotu");
+                tipcislo = int.Parse(Console.ReadLine());
+                if (tipcislo < Nahcislo)
+                {
+                    Console.WriteLine("to je malo");
+
+                }
+
+                else if (tipcislo > Nahcislo)
+                {
+                    Console.WriteLine("to je moc");
+
+                }
+                else
+                {
+                    Console.WriteLine("TREFA!!!!!!");
+                }
+
+
+            }
+            
+            
         }
     }
 }
